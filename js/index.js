5dedbaee152fc7d87110d30a3ee67ee941704faa
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-07-26 17:32:22 +0300

    did hotKeys, now user can clear all field by clickng 'C' key, save last painting by clicking 'S' and render last painting by clicking 'R'

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     drawingField.height = 400;
 
     let drawingCords = drawingField.getBoundingClientRect();
+    let cords = [];
 
     ctx.lineWidth = 10;
 
@@ -19,10 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
         isMouseDown = false;
 
         ctx.beginPath();
+
+        cords.push('mouseup');
     };
 
     const draw = (event) => {
         if (!isMouseDown) return;
+        cords.push([event.clientX - drawingCords.x, event.clientY - drawingCords.y]);
 
         ctx.lineTo(event.clientX - drawingCords.x, event.clientY - drawingCords.y);
         ctx.stroke();
@@ -36,9 +40,70 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const drawCircle = () => {
+        cords.push([event.clientX - drawingCords.x, event.clientY - drawingCords.y]);
+        cords.push('mouseup');
+
         ctx.arc(event.clientX - drawingCords.x, event.clientY - drawingCords.y, 5, 0, Math.PI * 2);
         ctx.fill();
         ctx.beginPath();
+    };
+
+    const clearField = (clearCords = true) => {
+        ctx.fillStyle = '#0d5747';
+        ctx.fillRect(0, 0, drawingField.width, drawingField.height);
+
+        ctx.beginPath();
+        ctx.fillStyle = 'black';
+
+        if (clearCords) {
+            cords = [];
+        }
+    };
+
+    const saveDrawingPath = () => {
+        localStorage.setItem('cords', JSON.stringify(cords));
+    };
+
+    const renderSavedPath = () => {
+        const savedCords = cords.slice();
+
+        let timer = setInterval(() => {
+            if (!savedCords.length) {
+                clearInterval(timer);
+                ctx.beginPath();
+                return;
+            }
+
+            const crd = savedCords.shift();
+
+            const currentCords = {
+                clientX: crd[0],
+                clientY: crd[1]
+            };
+
+            ctx.lineTo(currentCords.clientX, currentCords.clientY);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(currentCords.clientX, currentCords.clientY, 5, 0, Math.PI * 2);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.moveTo(currentCords.clientX, currentCords.clientY);
+        }, 15);
+    };
+
+    const hotKeys = event => {
+        if (event.keyCode === 67) {
+            clearField();
+        } else if (event.keyCode === 83) {
+            saveDrawingPath();
+        } else if (event.keyCode === 82) {
+            cords = JSON.parse( localStorage.getItem('cords') );
+        
+            clearField(false);
+            renderSavedPath();
+        }
     };
 
     drawingField.addEventListener('mousedown', startDraw);
@@ -46,4 +111,5 @@ document.addEventListener('DOMContentLoaded', () => {
     drawingField.addEventListener('click', drawCircle);
     drawingField.addEventListener('mousemove', draw);
     drawingField.addEventListener('mouseleave', endDraw);
+    document.addEventListener('keydown', hotKeys);
 });
